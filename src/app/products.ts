611d58705edeafcd82515a41e5f27cefe96f5fbc
Roman Biryukov <49877012+DJ-Haski@users.noreply.github.com>
2021-03-03 02:11:52 +0600

    Lab5

---
+++ src/app/products.ts
@@ -0,0 +1,268 @@
+export const products = [
+  { category_id: 0,
+    category_name: "Graphic cards",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Gigabyte Gv-N1660OC-6GD GeForce GTX 1660 OC 6G Graphics Card',
+      price: 573.99,
+      description: 'Series	GV-N1660OC-6GD',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Gigabyte-GeForce-Graphics-Windforce-Gv-N1660OC-6GD/dp/B07P76G428/ref=sr_1_1?crid=2V8DI0KGKQR6D&dchild=1&keywords=gtx+1660&qid=1614150309&sprefix=gtx+%2Caps%2C331&sr=8-1',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71Tw4qKcixL._AC_SL1500_.jpg',
+      likes: 240
+    },
+    {
+      id: 1,
+      name: 'Gigabyte Geforce GTX 1050 Ti OC 4GB GDDR5 128 Bit PCI-E Graphic Card (GV-N105TOC-4GD)',
+      price: 339.99,
+      description: 'NVIDIA GeForce GTX 1050 TI',
+      rating: 4.6,
+      link: 'https://www.amazon.com/OneOdio-Adapter-Free-Headphones-Professional-Telescopic/dp/B01N6ZJH96/ref=sr_1_1_sspa?dchild=1&keywords=headphones&qid=1614005830&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzM1AyRzRCRjk0Q1VZJmVuY3J5cHRlZElkPUEwOTgwNDM5M01GVFpHTU9RMVYxMSZlbmNyeXB0ZWRBZElkPUEwNTE3ODYwMk8wQkU3M0QxRkxFSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/810mHUuMxuL._AC_SL1500_.jpg',
+      likes: 305
+    },
+    {
+      id: 2,
+      name: 'Gigabyte GV-N207SGAMING OC-8GD GeForce RTX 2070 Super Gaming OC 8G Graphics Card, 3X Windforce Fans, 8GB 256-Bit GDDR6, Video Card',
+      price: 1229.99,
+      description: 'Powered by GeForce RTX 2070 super NVidia turing architecture & real time ray tracing Wind force 3x cooling system with alternate spinning fans ',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Popglory-Smartwatch-Pressure-Monitor-Fitness/dp/B08DXS7VCK/ref=sr_1_1_sspa?crid=1NDS4GJ8UAALT&dchild=1&keywords=apple+watch&qid=1614006308&sprefix=apple%2Caps%2C328&sr=8-1-spons&psc=1&smid=A3QEK68Z4UXCRO&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFORk5RV0c1SEREMUEmZW5jcnlwdGVkSWQ9QTA4NDAxNzMxU09WSzg4Qk1CUlRSJmVuY3J5cHRlZEFkSWQ9QTA4NjAyMDMyVVlVM01PV0YySURLJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71QQYvO%2BeKL._AC_SL1000_.jpg', 
+      likes: 102
+    },
+    {
+      id: 3,
+      name: 'GIGABYTE GeForce RTX 2080 Super Windforce OC 8G Graphics Card, 3X Windforce Fans, 8GB 256-Bit GDDR6, Gv-N208SWF3OC-8GD Video Card',
+      price: 1599.99,
+      description: 'Powered by GeForce RTX 2080 SUPER. Nvidia Turing architecture & Real time Ray Tracing.Aorus exclusive halo fan RGB.Windforce 3x cooling system with alternate spinning fans.Intuitive controls with AORUS engine.',
+      rating: 4.6,
+      link: 'https://www.amazon.com/GIGABYTE-GeForce-Windforce-Graphics-Gv-N208SWF3OC-8GD/dp/B07WQG7S5S?ref_=ast_sto_dp',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61NQDiQTIzL._AC_SL1000_.jpg',
+      likes: 545
+    },
+    {
+      id: 4,
+      name: 'Gigabyte GeForce GTX 1650 D6 OC 4G Graphics Card, 170mm Compact Size, 4GB 128-Bit GDDR6, GV-N1656OC-4GD Video Card',
+      price: 413.99,
+      description: 'Powered by GeForce GTX 1650.NVIDIA Turing architecture and GeForce Experience.Integrated with 4GB GDDR6 128bit memory interface.80mm unique blade fan.170 mm compact card size.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Gigabyte-GeForce-Graphics-Compact-GV-N1656OC-4GD/dp/B086TSK5M3?ref_=ast_sto_dp',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81CDN07ws8L._AC_SL1500_.jpg',
+      likes: 186
+    },
+    {
+      id: 5,
+      name: 'GIGABYTE GeForce GTX 1650 D6 Eagle OC 4G Graphics Card, 2X WINDFORCE Fans, 4GB 128-Bit GDDR6, GV-N1656EAGLE OC-4GD Video Card',
+      price: 1099.99,
+      description: 'Powered by GeForce GTX 1650.NVIDIA Turing architecture and GeForce Experience.Integrated with 4GB GDDR6 128-bit memory interface.WINDFORCE 2X Cooling System with alternate spinning fans.Core Clock: 1815 Mhz.',
+      rating: 4.3,
+      link: 'https://www.amazon.com/GIGABYTE-GeForce-Graphics-WINDFORCE-GV-N1656EAGLE/dp/B087R9H2KB?ref_=ast_sto_dp',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71SqrvifOfL._AC_SL1500_.jpg',
+      likes: 565
+    },
+    {
+      id: 6,
+      name: 'Gigabyte Gv-N165SWF2OC-4GD GeForce GTX 1650 Super Windforce OC 4G Graphics Card, 2X Windforce Fans, 4GB 128-Bit GDDR6, Video Card',
+      price: 579.99,
+      description: 'Powered by GeForce GTX 1650 Super integrated with 4GB GDDR6.Windforce 2x cooling system with alternate spinning fans.Intuitive controls with AORUS engine.4GB 128-bit GDDR6.1 x HDMI, 1 x DisplayPort.',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Gigabyte-GeForce-Windforce-Graphics-Gv-N165SWF2OC-4GD/dp/B081R359ZM?ref_=ast_sto_dp',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/811eJeQfu7L._AC_SL1500_.jpg',
+      likes: 145
+    },
+    {
+      id: 7,
+      name: 'PowerColor Red Devil AMD Radeon RX 5700 XT 8GB AXRX 5700XT 8GBD6-3DHE/OC',
+      price: 1599.99,
+      description: 'Radeon RX 5700 XT.RDNA architecture.7 nanometer technology.Radeon image sharpening.AMD FidelityFX Dual BIOS.PCIe 4.0.8GB GDDR6',
+      rating: 4.4,
+      link: 'https://www.amazon.com/PowerColor-Devil-Radeon-GDDR6-Graphics/dp/B07WP6TYQ3/ref=pd_sbs_1?pd_rd_w=fVXWj&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=A6HGNBJZ68T2WRFA97TE&pd_rd_r=92179ec5-45a5-489f-a8bc-2e5d73c0de7c&pd_rd_wg=VIUHj&pd_rd_i=B07WP6TYQ3&psc=1',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71i2Kqc1t2L._AC_SL1500_.jpg',
+      likes: 164
+    },
+    {
+      id: 8,
+      name: 'ASUS ROG Strix AMD Radeon RX 5600 XT TOP Edition Gaming Graphics Card (PCIe 4.0, 6GB GDDR6 Memory, HDMI, DisplayPort, 1080p Gaming, Axial-tech Fan Design, Auto-Extreme, Metal Backplate)',
+      price: 959.99,
+      description: 'TOP Edition: Boost Clock up to 1770 MHz (OC Mode)/ up to 1750 MHz (Gaming Mode).Axial-tech fan design features a smaller fan hub that facilitates longer blades and a barrier ring that increases downward air pressure.0dB Technology lets you enjoy light gaming in relative silence.MaxContact Technology allows 2X more contact with the GPU chip for improved thermal transfer.',
+      rating: 4.5,
+      link: 'https://www.amazon.com/ASUS-DisplayPort-Axial-tech-Auto-Extreme-Backplate/dp/B08C7L3Z13/ref=pd_sbs_2?pd_rd_w=3ooXE&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=QZDD7XYRD6E2T49HXM0D&pd_rd_r=7734b78a-9df1-4999-8991-b0dde2c98cba&pd_rd_wg=FoAaB&pd_rd_i=B08C7L3Z13&psc=1',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81eeOfCoSPL._AC_SL1500_.jpg',
+      likes: 1678
+    },
+    {
+      id: 9,
+      name: 'MSI Gaming Radeon RX 5600 XT Boost Clock: 1620 MHz 192-bit 6GB GDDR6 DP/HDMI Dual Torx 3.0 Fans Freesync DirectX 12 Ready Graphics Card (RX 5600 XT GAMING MX)',
+      price: 1099.99,
+      description: 'Chipset Radeon RX 5600 XT.Boost Clock 1620 MHz.Video Memory 6GB GDDR6.Memory Interface 192-bit.Output: DisplayPort x 3 (v1. 4) / HDMI 2. 0b x 1.',
+      rating: 4.6,
+      link: 'https://www.amazon.com/MSI-Radeon-RX-5600-XT/dp/B08CKDLN91/ref=pd_sbs_1?pd_rd_w=UYdr7&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=TXVVTDVSP2202Z3C1ZWQ&pd_rd_r=1fd88942-2abc-44a5-9d75-db81057b3dfa&pd_rd_wg=bZPkY&pd_rd_i=B08CKDLN91&psc=1',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61z-6ezAyLL._AC_SL1024_.jpg',
+      likes: 165
+    }]
+  },
+  { category_id: 1,
+    category_name: "Gaming Gear",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Logitech G502 Hero High Performance Gaming Mouse',
+      price: 48.50,
+      description: 'Hero 25K sensor through a software update from G HUB, this upgrade is free to all players: Our most advanced, with 1:1 tracking, 400+ ips, and 100 - 25,600 max dpi sensitivity plus zero smoothing, filtering, or acceleration',
+      rating: 4.6,
+      link: 'https://www.amazon.com/Logitech-G502-Performance-Gaming-Mouse/dp/B07GBZ4Q68/ref=sr_1_4?crid=30JN0HAY7RHAK&dchild=1&keywords=gaming+mouse&qid=1614707393&sprefix=gaming+mo%2Caps%2C838&sr=8-4',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51IOmsWQVAL._AC_SL1050_.jpg', 
+      likes: 1004
+    },
+    {
+      id: 1,
+      name: 'Razer DeathAdder Essential Gaming Mouse: 6400 DPI Optical Sensor - 5 Programmable Buttons - Mechanical Switches - Rubber Side Grips - White',
+      price: 29.99,
+      description: '',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Razer-DeathAdder-Essential-Gaming-Mouse/dp/B07Y693ND1/ref=sr_1_3?crid=30JN0HAY7RHAK&dchild=1&keywords=gaming+mouse&qid=1614707393&sprefix=gaming+mo%2Caps%2C838&sr=8-3',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51S6IQ2lGwL._AC_SL1500_.jpg', 
+      likes: 345
+    },
+    {
+      id: 2,
+      name: 'PICTEK Gaming Mouse Wired [7200 DPI] [Programmable] [Breathing Light] Ergonomic Game USB Computer Mice RGB Gamer Desktop Laptop PC Gaming Mouse, 7 Buttons for Windows 7/8/10/XP Vista Linux, Black',
+      price: 15.99,
+      description: '【Excellent gaming performance with 7200 DPI and 4 polling rate】 PICTEK T7 programmable gaming mice, default five DPI levels available from 1200/2400/3500/5500/7200 DPI. With two DPI button, you can adjust the dpi easily to get high accuracy and consistent responsiveness at any speed. 4 polling rate is adjustable: 125Hz/250Hz/500Hz/1000Hz, polling rate ensures smooth AND high-speed movement, up to 1000Hz makes it faster and more accurate than ordinary mouse.',
+      rating: 4.5,
+      link: 'https://www.amazon.com/PICTEK-Programmable-Breathing-Ergonomic-Computer/dp/B01FZ3BR5S/ref=sr_1_5?crid=30JN0HAY7RHAK&dchild=1&keywords=gaming+mouse&qid=1614707393&sprefix=gaming+mo%2Caps%2C838&sr=8-5',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61gxUsLtlyL._AC_SL1280_.jpg', 
+      likes: 12344
+    },
+    {
+      id: 3,
+      name: 'PICTEK TKL Mechanical Gaming Keyboard, RGB LED Rainbow Backlit 60% keyboard with Blue Switches, 27 LED Lighting Modes 87 Keys Keyboard, 100% Anti-Ghosting Tenkeyless Keyboard for Windows PC/MAC Games',
+      price: 28.99,
+      description: 'Pictek backlit keyboard is equipped with double-shot injection molded keycaps, which offers crystal clear uniform backlight and ensures lettering that doesnt scratch off. Features rainbow color, multiple lighting modes, 5 backlight brightness levels, 5 breathing speed, 2 special backlit desgined for RTS and FPS games, extending better gaming experience for gamers. The keys are NOT rgb but each zone is a different color red, yellow, green, blue, purple you cannot change them.',
+      rating: 4.6,
+      link: 'https://www.amazon.com/dp/B07VNQQZFK/ref=emc_b_5_i',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71eurRBO7iL._AC_SL1500_.jpg', 
+      likes: 1024
+    },
+    {
+      id: 4,
+      name: 'Pictek Gaming Keyboard and Mouse Combo, Wired LED RGB Backlit with Multimedia Keys Wrist Rest, 25 Keys Anti-Ghosting, 8 Efficient Multimedia Keys, Ergonomic Gaming Mouse with 4-Color Backlight for PC',
+      price: 11.29,
+      description: '【RGB Backlit Gaming Keyboard】7 Backlight modes& 4-degree adjustable lightness，can change of single color to 7-color and meets different needs during daytime/nighttime use. To avoid disturbing, press FN Page down to turn off the backlight during the night or during work.',
+      rating: 4.3,
+      link: 'https://www.amazon.com/dp/B07YWQGFFC/ref=emc_b_5_i',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71AlvM5iWWL._AC_SL1280_.jpg', 
+      likes:2004
+    }]
+  },
+  { category_id: 2,
+    category_name: "Headphones",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Logitech G432 DTS:X 7.1 Surround Sound Wired PC Gaming Headset (Leatherette)',
+      price: 58.00,
+      description: 'Large 50 millimeter audio drivers produce great sound for a more immersive gaming experience',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Logitech-Surround-Gaming-Headset-Leatherette/dp/B07MRMHML9/ref=sr_1_36?dchild=1&keywords=gaming&qid=1614713384&sr=8-36',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61TdnasnaaL._AC_SL1000_.jpg',
+      likes: 980
+    },
+    {
+      id: 1,
+      name: 'Razer Kraken Ultimate RGB USB Gaming Headset: THX 7.1 Spatial Surround Sound - Chroma RGB Lighting - Retractable Active Noise Cancelling Mic - Aluminum & Steel Frame - for PC - Classic Black',
+      price: 89.99,
+      description: 'The #1 Best-Selling Gaming Peripherals Manufacturer in the US: Source - The NPD Group, Inc. U.S. Retail Tracking Service, Keyboards, Mice, PC Headset/Pc Microphone, Gaming Designed, based on dollar sales, 2017-2021',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Razer-Kraken-Ultimate-Gaming-Headset/dp/B07ZK6R8JF/ref=sr_1_7?dchild=1&keywords=Manowar+headphones&qid=1614713052&sr=8-7',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/816okgFoTxL._AC_SL1500_.jpg',
+      likes: 8568
+    },
+    {
+      id: 2,
+      name: 'Razer Kraken Tournament Edition THX 7.1 Surround Sound Gaming Headset: Retractable Noise Cancelling Mic - USB DAC - For PC, PS4, PS5 Nintendo Switch, Xbox One, Xbox Series X, & S, Mobile – Green',
+      price: 69.65,
+      description: 'The #1 Best-Selling Gaming Peripherals Manufacturer in the US: Source - The NPD Group, Inc. U.S. Retail Tracking Service, Keyboards, Mice, PC Headset/Pc Microphone, Gaming Designed, based on dollar sales, 2017-2021',
+      rating: 4.8,
+      link: 'https://www.amazon.com/dp/B07G5FQMJ8?ref=emc_p_m_5_i',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61Ufzi3lugL._AC_SL1440_.jpg',
+      likes: 1245
+    },
+    {
+      id: 3,
+      name: 'HyperX Cloud II - Gaming Headset, 7.1 Surround Sound, Memory Foam Ear Pads, Durable Aluminum Frame, Detachable Microphone, Works with PC, PS4, Xbox One - Red',
+      price: 128.81,
+      description: 'Designed for comfort: Exceptionally comfortable memory foam ear cushions and padded leatherette headband help keep you focused on gaming',
+      rating: 4.4,
+      link: 'https://www.amazon.com/HyperX-Cloud-Gaming-Headset-KHX-HSCP-RD/dp/B00SAYCXWG/ref=sr_1_19?crid=1V5FIV7PB98US&dchild=1&keywords=headset&qid=1614713267&sprefix=heads%2Caps%2C479&sr=8-19',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61JJl260NlL._AC_SL1412_.jpg',
+      likes: 4235
+    },
+    {
+      id: 4,
+      name: 'Corsair HS60 PRO - 7.1 Virtual Surround Sound Gaming Headset with USB DAC - Works with PC, Xbox Series X, Xbox Series S, Xbox One, PS5, PS4, and Nintendo Switch - Carbon (CA-9011213-NA)',
+      price: 49.00,
+      description: 'Adjustable ear cups fitted with plush memory foam provide exceptional comfort for hours of gameplay High-quality custom-tuned 50mm neodymium',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Corsair-HS60-Pro-Headphones-Compatible/dp/B07X9W8CBP/ref=sxin_11_ac_d_rm?ac_md=2-2-Y29yc2FpciBoZWFkc2V0-ac_d_rm&crid=1V5FIV7PB98US&cv_ct_cx=headset&dchild=1&keywords=headset&pd_rd_i=B07X9W8CBP&pd_rd_r=804053b3-7015-45d1-bba8-e34102629af9&pd_rd_w=fHrIb&pd_rd_wg=DT1gQ&pf_rd_p=876e1cf2-5a60-4de1-a37e-e4e3a1c07041&pf_rd_r=GQGPXVABT0JNJGX5CZV5&psc=1&qid=1614713188&sprefix=heads%2Caps%2C479&sr=1-1-12d4272d-8adb-4121-8624-135149aa9081',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61FG641dbtL._AC_SL1500_.jpg',
+      likes: 768
+    }]
+  },
+  { category_id: 3,
+    category_name: "Other",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Mr IRONSTONE L-Shaped Desk 50.8" Computer Corner Desk, Home Gaming Desk, Office Writing Workstation with Large Monitor Stand, Space-Saving, Easy to Assemble, Black',
+      price: 79.99,
+      description: 'Nearly full L Shaped Desk with Optional Space Saving Monitor Shelf: Nearly full L Shape design, make the most of your space, easy to fit snugly in a corner to maximize your limited space. Space-save monitor shelf of l desk for your monitors leaves plenty of surface space for your keyboard and mouse when writing, gaming, computer work, and other home office activities.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Mr-IRONSTONE-L-Shaped-Workstation-Space-Saving/dp/B082Y16QZ2/ref=sr_1_2?dchild=1&keywords=gaming&qid=1614713352&sr=8-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71OrdSFSHeL._AC_SL1500_.jpg',
+      likes: 456
+    },
+    {
+      id: 1,
+      name: 'ASUS TUF Gaming 23.6" 1080P Curved Monitor (VG24VQE) - Full HD, 165Hz, 1ms, Extreme Low Motion Blur, Speaker, Adaptive-Sync, FreeSync Premium, Shadow Boost, VESA Mountable, DisplayPort, HDMI',
+      price: 179.00,
+      description: '23.6-inch Full HD (1920 x 1080) 1500R gaming monitor with ultrafast 165Hz refresh rate (supports 144Hz) designed for professional gamers and immersive gameplay',
+      rating: 4.8,
+      link: 'https://www.amazon.com/ASUS-Gaming-Curved-Monitor-VG24VQE/dp/B08K2DSMJV/ref=sr_1_4?dchild=1&keywords=gaming&qid=1614713384&sr=8-4',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81GL%2BSz8RkL._AC_SL1500_.jpg',
+      likes: 8786
+    },
+    {
+      id: 2,
+      name: 'Blade Hawks RGB Gaming Mouse Pad, LED Soft Extra Extended Large Mouse Pad, Anti-Slip Rubber Base, Computer Keyboard Mouse Mat - 31.5 X 12 Inch',
+      price: 28.58,
+      description: 'Measuring 800 x 300 mm/ 31. 5×12 inches, which is wider and taller than others. Mouse pad can perfectly fit your keyboard and mouse of any size, and there is also ample space for peripherals such as phones, tablets, etc.',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Blade-Hawks-Extended-Lighting-Anti-slip/dp/B07L4BGL3D/ref=sr_1_8?dchild=1&keywords=gaming&qid=1614713384&sr=8-8',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51ybekXvQLL._AC_SL1000_.jpg',
+      likes: 457
+    },
+    {
+      id: 3,
+      name: 'KBEST Massage Gaming Chair - High Back Racing PC Computer Desk Office Chair Swivel Ergonomic Executive Leather Chair with Adjustable Back Angle, Armrests and Footrest, Gray/Black',
+      price: 148.99,
+      description: 'RACING STYLE & MULTIFUNCTIONS: Innovative implant USB electric massager inside the lumbar cushion effectively helps you relieve fatigue; Retractable padded footrest, 360° swivel base, smooth rollers, adjustable armrests, height, and back reclining angles make it a decent gaming chair with an excellent recline locking system to lock the back between 90°~135°',
+      rating: 4.6,
+      link: 'https://www.amazon.com/KBEST-Massage-Gaming-Chair-Adjustable/dp/B08QD91TXC/ref=sr_1_10?dchild=1&keywords=gaming&qid=1614713384&sr=8-10',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61to04Qm0hL._AC_SL1484_.jpg',
+      likes: 235
+    },
+    {
+      id: 4,
+      name: 'PC Gaming Headset Headphone Hook Holder Hanger Mount, Headphones Stand with Adjustable & Rotating Arm Clamp , Under Desk Design , Universal Fit , Built in Cable Clip Organizer EURPMASK',
+      price: 14.99,
+      description: 'UNIVERSAL UNDER DESK DESIGN clamps on to desks up to 37mm thick and as narrow as 7mm for a near universal fit on most desks || Spring Clamp is easy to remove and reattach!',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Headphone-Headphones-Adjustable-Universal-EURPMASK/dp/B07BVK2FQW/ref=sr_1_33?dchild=1&keywords=gaming&qid=1614713384&sr=8-33',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/616qQCQHs2L._AC_SL1000_.jpg',
+      likes: 4124
+    }]
+  }
+];
